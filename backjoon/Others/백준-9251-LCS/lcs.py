cbdddb3eebddb2bba72e08ca30470e998131dffc
htchoi1006@kookmin.ac.kr <choihyeoktae@choehyeogtaeui-MacBookPro.local>
2022-05-04 14:32:02 +0900

    Others

---
+++ backjoon/Others/백준-9251-LCS/lcs.py
@@ -0,0 +1,17 @@
+import sys
+
+string1 = sys.stdin.readline().strip().upper()
+string2 = sys.stdin.readline().strip().upper()
+
+lcs = [[0]*(len(string2)+1) for _ in range(len(string1)+1)]
+
+for i in range(1, len(string1)+1):
+    for j in range(1, len(string2)+1):
+        if(string1[i-1] == string2[j-1]):
+            lcs[i][j] = lcs[i-1][j-1] + 1 
+        else:
+            lcs[i][j] = max(lcs[i - 1][j], lcs[i][j - 1])
+
+
+    
+print(lcs[-1][-1])
