bb7b4e56cfe7ecf0080bca6b20ef3e2fa74bbacf
htchoi1006@kookmin.ac.kr <htchoi1006@kookmin.ac.kr>
2022-07-06 17:38:56 +0900

    .

---
+++ backjoon/Bronze I/[1356] 유진수/solution.py
@@ -0,0 +1,20 @@
+n = input()
+n_len = len(n)
+true = 0
+
+for i in range(n_len - 1):
+    left = right = 1
+    
+    for j in range(i + 1):
+        left *= int(n[j])
+    
+    for k in range(i+1, n_len):
+        right *= int(n[k])
+        
+    if(left == right):
+        print("YES")
+        true = 1
+        break
+    
+if(true == 0):
+    print("NO")
