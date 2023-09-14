b519c5c70db020210cf957cc4b9874b1c4fe50b1
htchoi1006 <htchoi1006@kookmin.ac.kr>
2023-09-14 21:38:23 +0900

    .

---
+++ backjoon/Gold/[5430]AC.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+t = int(input())
+
+for _ in range(t):
+    cmd = input()
+    n = int(input())
+    arr = input()[1:-1].split(',')
+    
+    queue = deque(arr)
+    flag = 0
+    
+    if(n == 0):
+        queue = []
+        
+    for i in cmd:
+        if(i == 'R'):
+            flag += 1
+        elif(i == 'D'):
+            if(len(queue) == 0):
+                print('error')
+                break
+            else:
+                if(flag % 2 == 0):
+                    queue.popleft()
+                else:
+                    queue.pop()
+                    
+
+    else:
+        if(flag % 2 == 0):
+            print("[" + ",".join(queue) + ']')
+        else:
+            queue.reverse()
+            print("[" + ",".join(queue) + ']')
