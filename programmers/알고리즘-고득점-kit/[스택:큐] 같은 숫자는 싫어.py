6d056dcc873491dc1f522000d7fbc5ef091f2a62
Choi Hyuk Tae <40112123+htchoi1006@users.noreply.github.com>
2024-03-04 17:18:34 +0900

    Update [스택:큐] 같은 숫자는 싫어.py

--- programmers/알고리즘-고득점-kit/[스택:큐] 같은 숫자는 싫어.py
+++ programmers/알고리즘-고득점-kit/[스택:큐] 같은 숫자는 싫어.py
@@ -3,9 +3,7 @@ def solution(arr):
     answer.append(arr[0])
 
     for i in range(1, len(arr)):  
-        if arr[i] == arr[i-1]:  #앞의 수와 중복되면 패스
-            continue
-        else:  #앞의 수와 다르면 배열에 추가
+        if arr[i] != arr[i-1]:  
             answer.append(arr[i])
                     
     
